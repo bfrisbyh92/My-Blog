3f778138f9f21a8e4c79c78cb63a4984ed2389aa
bfrisbyh92 <bfrisbyh92@gmail.com>
2023-01-30 16:38:39 -0500

    Changed colors and background image.

--- pages/_app.js
+++ pages/_app.js
@@ -7,10 +7,11 @@ function MyApp({ Component, pageProps }) {
       <div
         className="h-screen w-screen fixed m-auto inset-0 bg-center bg-no-repeat bg-cover transition opacity-100"
         style={{
-          backgroundImage: `url("./assets/images/hero-section-bg-dark.webp")`,
+          // backgroundImage: `url("public/assets/images/bg.webp")`,
+          backgroundImage: `url("./assets/images/hero-section-bg-dark2.webp")`,
         }}
       ></div>
-      <NextNProgress color="#f472b6" />
+      <NextNProgress color="#76aaea" />
       <Component {...pageProps} />
     </div>
   );
